7b27d7ccc705fe76650dc5bdf99e72329e8bccab
David Freelan <dfreelan@vss-mbp-4.mesa.gmu.edu>
2015-04-11 15:42:06 -0400

    this doesnt work at all

--- rl-sean.lisp
+++ rl-sean.lisp
@@ -1,6 +1,4 @@
-
-
-;;; FUNCTION DICTIONARY
+;;e FUNCTION DICTIONARY
 ;;
 ;;
 ;;
@@ -70,6 +68,7 @@ to provide the current alpha value."
 	nil
   )
   (dprint reward "you canned q-learner with reward as:")
+  (setf *debug* nil)
   ;;equation is (qtable[oldstate][action] = (1-alpha) * qtable[oldstate][action] + alpha*(reward + gamma* (max-q q-table current-state) )
   
   (setf alpha (funcall alpha-func iteration))
@@ -142,7 +141,7 @@ to provide the current alpha value."
 
 				(if (<= current-state 0)
 					
-					(setf q-table (q-learner q-table -1 old-state my-action-taken current-state gamma alpha-func i));;losing update
+					(setf q-table (q-learner q-table 1 old-state my-action-taken current-state gamma alpha-func i));;losing update
 					(progn
 						;;else we continue on, let the "opponent" make a move
 						;;take an action (call max-action state)
@@ -155,7 +154,7 @@ to provide the current alpha value."
 				
 						;;check if the opponent lost, if so then do a winning-update
 						(if (<= current-state 0)
-							(progn (if (< (- old-state current-state) 2) (print (- old-state current-state)) nil)  (setf q-table (q-learner q-table 1 old-state my-action-taken current-state gamma alpha-func i)) );;winning update
+							(progn (if (< (- old-state current-state) 2) (print (- old-state current-state)) nil)  (setf q-table (q-learner q-table -1 old-state my-action-taken current-state gamma alpha-func i)) );;winning update
 							;;if not the game is still going, learn from future rewards!
 							(setf q-table (q-learner q-table 0 old-state my-action-taken current-state gamma alpha-func i));;future rewards update
 						)
@@ -179,6 +178,15 @@ to provide the current alpha value."
      	(when (and (numberp result) (<= result 3) (>= result 1))
       	 (return result))
      	(format t "~%Please select no fewer than 1 and no more than 3! ~%How many sticks would you like to take?~%"))))
+(defun make-user-move-3-nim ()
+  "Returns the number of sticks the user wants to remove"
+  (let ((result))
+    (loop
+			(setf result (read))
+     	(when (and (numberp result) (<= result 9) (>= result 1))
+      	 (return result))
+     	(format t "~%Please select no fewer than 1 and no more than 9! ~%How many sticks would you like to take?(1,2,3 = take 1 2 or 3 from first pile, 4 5 6= take 1 2 3 from second pile, etc~%"))))
+
 
 (defun play-nim (q-table max-heap-size)
   "Plays a game of nim.  Asks if the user wants to play first,
@@ -205,23 +213,94 @@ them wins.  Reports the winner."
 						(if (< current-state 1) (format t "~%YOU LOSE!!!~%") nil)))
 				)))))
 
+
+(defun play-3-nim (heap-sizes q-table)
+  "Returns a q-table after learning how to play nim"
+   
+  ;;make q table, ignore sean only do size 20.
+  (let ((num-iterations 1)(q-table (make-q-table (+ (* (+ (first heap-sizes) 1) (+ (second heap-sizes) 1) (+ (third heap-sizes) 1))  1) 9)))
   
+    ;;loop 1 to iterations
+	(dotimes (i num-iterations)
+		;;set state to heap-size
+		(let ((current-state (get-state-from-stacks heap-sizes heap-sizes)) (old-state 0))
+			;;loop until game finished
+			(setf current-state (get-state-from-stacks heap-sizes heap-sizes))
+			(loop while (> current-state 0) do
+				
+			
+				;;before taking an action, record current state for later use
+				(setf old-state current-state)
+				;;take an action (call max-action state)
+				(setf my-action-taken  (max-action q-table current-state) )
+				(dprint my-action-taken "action taken:")
+				
+				;;randomly do something a bit different
+
+				(setf my-action-taken (mod my-action-taken 9))
+	
+				;; calculate current state (modify current-state)
+				;;(setf current-state (- (- current-state my-action-taken) 1))
+				(dprint current-state "current-state passing in")
+				(dprint heap-sizes "heap-sizes passing in") 
+				(setf current-state (calculate-new-state current-state my-action-taken heap-sizes))
+				;;check to see if you've lost, if so then do a losing-update
+				(if (<= current-state 0)
+					
+					(print "you win!");;losing update
+					(progn
+						;;else we continue on, let the "opponent" make a move
+						;;take an action (call max-action state)
+						(print "current stacks are below, how many do you want to take?")
+						(print (calculate-3d-from-1d current-state heap-sizes))
+						(setf opp-action-taken  (- (make-user-move-3-nim ) 1))
+						(dprint opp-action-taken "action taken:")
+				
+						;; calculate current state (modify current-state)
+						;;(setf current-state (- (- current-state opp-action-taken) 1))
+						(setf current-state (calculate-new-state current-state opp-action-taken heap-sizes))
+						(print (calculate-3d-from-1d current-state heap-sizes))
+	
+						;;check if the opponent lost, if so then do a winning-update
+						(if (<= current-state 0)
+							(progn   (print "you lose :[ ") );;winning update
+							;;if not the game is still going, learn from future rewards!
+							nil)
+						;;else do a regular old update.
+				)))))
+				q-table))
+
+
+ 
 (defun best-actions (q-table)
   "Returns a list of the best actions.  If there is no best action, this is indicated with a hyphen (-) NOTE: our assignment starts from state 0 to state n, where n is the number of sticks"
 	(let ((action-list '()))  
 	  (dotimes (i (num-states q-table))
-		(setf max-action (+ (max-action q-table i 1000) 1))
-		
-		(if (> max-action 4)
+		(setf max-action (max-action q-table i 1000000))
+		;;(print max-action)	
+		(if (> max-action 999999)
 			(setf max-action '-)
 			nil)
-		(if  (< (max-q q-table i) 0)
+		(if  (<= (max-q q-table i) 0)
 			(setf max-action '-)
 			nil)
 		(setf action-list (append action-list (list max-action))))
 	  action-list))
 
-  
+ (defun best-actions-3-nim (q-table)
+  "Returns a list of the best actions.  If there is no best action, this is indicated with a hyphen (-) NOTE: our assignment starts from state 0 to state n, where n is the number of sticks"
+	(let ((action-list '()))  
+	  (dotimes (i (num-states q-table))
+		(setf max-action (+ (max-action q-table i) 1))
+		;;(print max-action)	
+		(if (> max-action 999999)
+			(setf max-action '-)
+			nil)
+		(if  (<= (max-q q-table i) 0)
+			(setf max-action '-)
+			nil)
+		(setf action-list (append action-list (list max-action))))
+	  action-list)) 
 
 ;; example:
 ;; 
@@ -240,6 +319,141 @@ them wins.  Reports the winner."
 #|
 
 |#
+;;3d-> 1d conversion, print out the state given the heap sizes
+(defun get-state-from-stacks (max-heap-sizes heap-sizes)
+	(setf max-heap-sizes (copy-list max-heap-sizes))
+	(incf (first max-heap-sizes))
+	(incf (second max-heap-sizes))
+	(incf (third max-heap-sizes))	
+	(+ (first heap-sizes) (* (second heap-sizes) (first max-heap-sizes)) (* (third heap-sizes) (first max-heap-sizes) (second max-heap-sizes)))
+)
+;;1d->3d->1d (get in state, convert to heap sizes, apply action, return converted state number)
+(defun calculate-new-state (current-state action max-heap-sizes)
+	(setf max-heap-sizes (copy-list max-heap-sizes))
+	(incf (first max-heap-sizes))
+	(incf (second max-heap-sizes))
+	(incf (third max-heap-sizes))
+	(let ((counter 0) (stack-num (floor action 3)) (heap-numbers '(0 0 0)))
+		(dprint "current state is:")
+		(dprint current-state)
+		(setf  (third heap-numbers) (floor current-state (* (first max-heap-sizes) (second max-heap-sizes))))
+		
+		(setf current-state (- current-state (* (floor current-state (* (first max-heap-sizes) (second max-heap-sizes))) (* (first max-heap-sizes) (second max-heap-sizes)))))
+		(dprint "after doing the third dimesion current -state is now")
+		(dprint current-state)
+		(dprint "here's the amount we subtract")
+		(dprint (* (first max-heap-sizes) (second max-heap-sizes)))
+		(setf (second heap-numbers) (floor current-state (first max-heap-sizes)))
+
+		(setf current-state (- current-state (* (floor current-state (first max-heap-sizes)) (first max-heap-sizes))))
+		(dprint "after the second subtract, current state is")
+		(dprint current-state)
+		(setf (first heap-numbers) current-state)
+		;;this is absoulutely terrible lisp or even code in general. 
+		;; our goal is that if the action was invalid, we hve some rational way of choosing a new stack, this stack to pick from will be 
+		;; the left most non-empty, with wrap-around
+		(loop while (or (< counter 3) (<= (nth stack-num heap-numbers) 0)) do (progn 
+			(setf stack-num (mod (- stack-num 1) 3))
+			(incf counter)
+		))
+		
+		(setf (nth stack-num heap-numbers) (- (nth stack-num heap-numbers) (+ (mod action 3) 1))) 
+		(if (< (nth stack-num heap-numbers) 0)
+			(setf (nth stack-num heap-numbers) 0)
+			nil)
+		;;our returned value
+		(get-state-from-stacks (list (decf (first max-heap-sizes)) (decf (second max-heap-sizes)) (decf (third max-heap-sizes))) heap-numbers)))
+;;returns the stacks given the state and max heap sizes
+(defun calculate-3d-from-1d (current-state max-heap-sizes)
+	(setf max-heap-sizes (copy-list max-heap-sizes))
+	(incf (first max-heap-sizes))
+	(incf (second max-heap-sizes))
+	(incf (third max-heap-sizes))
+	(let ((counter 0) (heap-numbers '(0 0 0)))
+		(dprint "current state is:")
+		(dprint current-state)
+		(setf  (third heap-numbers) (floor current-state (* (first max-heap-sizes) (second max-heap-sizes))))
+		
+		(setf current-state (- current-state (* (floor current-state (* (first max-heap-sizes) (second max-heap-sizes))) (* (first max-heap-sizes) (second max-heap-sizes)))))
+		(dprint "after doing the third dimesion current -state is now")
+		(dprint current-state)
+		(dprint "here's the amount we subtract")
+		(dprint (* (first max-heap-sizes) (second max-heap-sizes)))
+		(setf (second heap-numbers) (floor current-state (first max-heap-sizes)))
+
+		(setf current-state (- current-state (* (floor current-state (first max-heap-sizes)) (first max-heap-sizes))))
+		(dprint "after the second subtract, current state is")
+		(dprint current-state)
+		(setf (first heap-numbers) current-state)
+		(print "resulting heap-numbers")
+		(print heap-numbers)))
+
+(defun learn-3-nim (heap-sizes gamma alpha-func num-iterations)
+  "Returns a q-table after learning how to play nim"
+ 
+  ;;make q table, ignore sean only do size 20.
+  (let ((q-table (make-q-table (+ (* (+ (first heap-sizes) 1) (+ (second heap-sizes) 1) (+ (third heap-sizes) 1))  1) 9)))
+  
+    ;;loop 1 to iterations
+	(dotimes (i num-iterations)
+		;;set state to heap-size
+		(let ((current-state (get-state-from-stacks heap-sizes heap-sizes)) (old-state 0))
+			;;loop until game finished
+			(setf current-state (get-state-from-stacks heap-sizes heap-sizes))
+			(loop while (> current-state 0) do
+				(dprint current-state "current state is:")
+				;;before taking an action, record current state for later use
+				(setf old-state current-state)
+				;;take an action (call max-action state)
+				(setf my-action-taken  (max-action q-table current-state) )
+				(dprint my-action-taken "action taken:")
+				
+				;;randomly do something a bit different
+				(if  (=  (random 2) 0)
+					;; do something other than optimal
+					(if (= (random 2) 0);;we don't really need to split this into two conditions since we have a modulus, but whatever
+						(setf my-action-taken (- my-action-taken (+ (random 4) 1)))
+						(setf my-action-taken (+ my-action-taken (- (random 4) 1)))
+					)nil)
+				(setf my-action-taken (mod my-action-taken 9))
+	
+				;; calculate current state (modify current-state)
+				;;(setf current-state (- (- current-state my-action-taken) 1))
+				(dprint current-state "current-state passing in")
+				(dprint heap-sizes "heap-sizes passing in") 
+				(setf current-state (calculate-new-state current-state my-action-taken heap-sizes))
+				;;check to see if you've lost, if so then do a losing-update
+				(if (<= current-state 0)
+					
+					(setf q-table (q-learner q-table -1 old-state my-action-taken current-state gamma alpha-func i));;losing update
+					(progn
+						;;else we continue on, let the "opponent" make a move
+						;;take an action (call max-action state)
+						(setf opp-action-taken  (max-action q-table current-state) )
+						(dprint opp-action-taken "action taken:")
+						(if  (=  (random 2) 0)
+							;; do something other than optimal
+							(if (= (random 2) 0);;we don't really need to split this into two conditions since we have a modulus, but whatever
+							(setf opp-action-taken (- opp-action-taken (+ (random 4) 1)))
+							(setf opp-action-taken (+ opp-action-taken (- (random 4) 1)))
+						)nil)
+						(setf opp-action-taken (mod opp-action-taken 9))	
+	
+						;; calculate current state (modify current-state)
+						;;(setf current-state (- (- current-state opp-action-taken) 1))
+						(setf current-state (calculate-new-state current-state opp-action-taken heap-sizes))
+						;;check if the opponent lost, if so then do a winning-update
+						(if (<= current-state 0)
+							(progn  (setf q-table (q-learner q-table 1 old-state my-action-taken current-state gamma alpha-func i)));;winning update
+							;;if not the game is still going, learn from future rewards!
+							(setf q-table (q-learner q-table 0 old-state my-action-taken current-state gamma alpha-func i));;future rewards update
+						)
+						;;else do a regular old update.
+				)))))
+				q-table))
+
+
+
 (defun test-max-action ()
 	(let ((qtable (make-q-table 2 2)))
 		(setf (aref qtable 0 1) 1)
@@ -254,4 +468,26 @@ them wins.  Reports the winner."
 	(play-nim *q-table* 22))
 
 
-(base-assignment)
+(defun test-bin-taking ()
+
+;;get-state-from-stacks (max-heap-sizes heap-sizes)
+;;(defun calculate-3d-from-1d (current-state max-heap-sizes)
+(let ((one-d-state 0) (three-d-state '(3 3 3)))
+	(print (setf one-d-state (get-state-from-stacks '(2 2 2) three-d-state)))
+	(print (setf three-d-sttate (calculate-3d-from-1d one-d-state '(3 3 3))))
+	(print "END")
+	;;(defun calculate-new-state (current-state action max-heap-sizes)
+	(dotimes (i 9)	
+		(print (calculate-3d-from-1d (calculate-new-state one-d-state i '(3 3 3)) '(3 3 3))) 
+	)
+))
+;;(base-assignment)
+(setf *debug* nil)
+
+(test-bin-taking)
+;;(setf *q-table* (learn-3-nim '(3 3 3) .1 #'basic-alpha 90000))
+
+;;(print *q-table*)
+;;(print (best-actions-3-nim *q-table*))
+;;(play-3-nim '(3 3 3) *q-table*)
+;;(base-assignment)
